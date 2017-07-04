57b1ecc898718a9f7581f855762598ed8ff08620
Julio Carlos Menendez <hello@juliomenendez.com>
2017-07-04 07:45:25 -0700

    Allows using absolute URLs. Refs #31

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var templateUrlRegex = /templateUrl\s*:(\s*['"`](.*?)['"`]\s*([,}]))/gm;
 var stylesRegex = /styleUrls *:(\s*\[[^\]]*?\])/g;
 var stringRegex = /(['`"])((?:[^\\]\\\1|.)*?)\1/g;
 
-function replaceStringsWithRequires(string) {
+function replaceStringsWithRequires(string, useAbsoluteUrls) {
   return string.replace(stringRegex, function (match, quote, url) {
-    if (url.charAt(0) !== ".") {
+    if (!useAbsoluteUrls && url.charAt(0) !== ".") {
       url = "./" + url;
     }
     return "require('" + url + "')";
@@ -33,6 +33,8 @@ module.exports = function(source, sourcemap) {
       templateProperty = 'templateUrl';
   }
 
+  var useAbsoluteUrls = config.useAbsoluteUrls || false;
+
     // Not cacheable during unit tests;
   this.cacheable && this.cacheable();
 
@@ -41,14 +43,14 @@ module.exports = function(source, sourcemap) {
                  // with: template: require('./path/to/template.html')
                  // or: templateUrl: require('./path/to/template.html')
                  // if `keepUrl` query parameter is set to true.
-                 return templateProperty + ":" + replaceStringsWithRequires(url);
+                 return templateProperty + ":" + replaceStringsWithRequires(url, useAbsoluteUrls);
                })
                .replace(stylesRegex, function (match, urls) {
                  // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
                  // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
                  // or: styleUrls: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
                  // if `keepUrl` query parameter is set to true.
-                 return styleProperty + ":" + replaceStringsWithRequires(urls);
+                 return styleProperty + ":" + replaceStringsWithRequires(urls, useAbsoluteUrls);
                });
 
   // Support for tests
